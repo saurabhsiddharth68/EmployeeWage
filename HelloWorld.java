5bf0ccbf6d08c35218d7bfdb9906761639ef1f4c
Raj Kumar Rao <https://github.com/saurabhsiddharth68>
2021-10-03 23:21:17 +0530

    update file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World India");
+		System.out.println("Hello India");
+		System.out.println("Hello UK");
 	}
 }
